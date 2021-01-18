16f05f1f1941191b7ac624c1259712d23adc5280
RailonA <railonacosta@gmail.com>
2021-01-18 09:37:14 -0500

    Solving issue number 1

--- drawing_book.rb
+++ drawing_book.rb
@@ -1,12 +1,37 @@
+
+
 def pageCount(n, p)
-    #
-    # Write your code here.
-    #
+
+   total_pages = n
+   goal_page = p
+   
+
+   if n > p 
+    if p > (n/2).round
+        #start from the end 
+    else 
+
+     page_counter = (p / 2).round
+    end
+   elsif n == p
+    page_counter = 0
+
+        
+    
+
+
+#    page_array = [n..p]
+
+#  puts start_page
+  puts page_counter
+
+
+
 
 end
 
 
-pageCount(5,3)
+pageCount(6,2)
 
 
 
