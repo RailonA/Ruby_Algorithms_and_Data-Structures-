2b8ab100eea9cc90e72d88e5958bbbd322a4a6a6
RailonA <railonacosta@gmail.com>
2021-03-10 09:28:05 -0500

    Add more problems

--- electronic_shop.rb
+++ electronic_shop.rb
@@ -1,40 +1,39 @@
 
 
 def getMoneySpent(keyboards, drives, b)
-    #
-    # Write your code here.
-    #
-    #  loop through keyboards
-    #  compare keyboards[0] + drives[i]
-    #  compare array_1[index 2] + drives[i]
+  #
+  # Write your code here.
+  #
+  #  loop through keyboards
+  #  compare keyboards[0] + drives[i]
+  #  compare array_1[index 2] + drives[i]
 
-     total = 0 
-     last_t = 0
+   total = 0 
+   last_t = 0
 
 
-     if keyboards.min + drives.min > b
-         total = -1
-         total   
-     else
-        
-     keyboards.each do |item |
-        drives.each do |usb|
-         # puts keyboards[item], drives[usb]
-        
-        total = item+usb
-        if total >= last_t && total <= b
-        last_t = total
-      end
-      end
+   if keyboards.min + drives.min > b
+       total = -1
+       total   
+   else
+      
+   keyboards.each do |item |
+      drives.each do |usb|
+       # puts keyboards[item], drives[usb]
+      
+      total = item+usb
+      if total >= last_t && total <= b
+      last_t = total
     end
+    end
+  end
 
-     last_t
+   last_t
 
-     end
-     
-
-
+   end
 end
+
+
 
 getMoneySpent([4],[5],5)
 
---
+++ picking_numbers.rb
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/picking-numbers/problem
+
+def pickingNumbers(a)
+    # Write your code here
+
+end
