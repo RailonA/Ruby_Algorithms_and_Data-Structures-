1da31964089fd2e2f3c4102116a48a334fe1fe9e
RailonA <railonacosta@gmail.com>
2021-03-12 08:49:37 -0500

    Solve repeated string

--- picking_numbers.rb
+++ picking_numbers.rb
@@ -1,6 +1,31 @@
 # https://www.hackerrank.com/challenges/picking-numbers/problem
 
-def pickingNumbers(a)
-    # Write your code here
+def repeatedString(s, n)
 
-end
+    counter = 0 
+    remainder = 0 
+
+    counter = n/s.size
+    remainder = n % s.size
+ 
+    puts counter
+    puts remainder
+   
+
+#    puts s.flat_map { |item| Array.new(counter, item) }
+  puts counter.times { s    }
+
+end    # Write your code here
+
+
+
+
+
+repeatedString(['a','b','a'], 10)
+
+# a b a  a b a  a b a  a
+
+# a = 7 
+
+# 10 / 3
+# 3 r 1
--- repeated_string.rb
+++ repeated_string.rb
@@ -1,1 +1,20 @@
 #  https://www.hackerrank.com/challenges/repeated-string/problem
+
+
+
+
+def repeatedString(s, n)
+
+    counter = n/s.size
+    remainder = n % s.size
+   
+   c1 =  s.count('a')*counter
+   c2 = s[0...remainder].count('a')
+
+    result = c1+c2
+
+  puts  result
+
+end
+
+repeatedString(['a','b','a'], 10)
