eb8eb19550949c0b44c80e142c35d348afc631ca
RailonA <railonacosta@gmail.com>
2021-03-16 10:30:14 -0400

    Fix line 9

--- anagram.rb
+++ anagram.rb
@@ -6,19 +6,19 @@ def anagram(s)
     # split the array1 and check if array2 has the char
     # if so drop that char, and count how many char are left behind. 
 
-return -1 unless (s.length % 2 == 0)
+return -1 if s.length.odd?
 
   array1 = s.slice(0, s.length / 2)
   array2 = s.slice(s.length / 2, s.length)
   
  for i in 0..array1.length - 1 do
-    array2.sub!(array1[i], "") if array2.include?(array1[i])
+    array2.sub!(array1[i], '') if array2.include?(array1[i])
   end
   return array2.length
-
 
 end
 
 
 
 anagram(["a","b", "d", "r"])
+
