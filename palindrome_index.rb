27c453d4a770310e0edbeca23342bd32bd42c998
RailonA <railonacosta@gmail.com>
2021-05-18 13:17:00 -0400

    Add Palindrom index

--- palindrome_index.rb
+++ palindrome_index.rb
@@ -1,2 +1,25 @@
 # https://www.hackerrank.com/challenges/palindrome-index/problem
 
+def palindromeIndex(s)
+
+i = 0 
+j = s.size-1
+
+    while i < j && s[i] == s[j] 
+        i+= 1
+        j-=1
+    end
+
+    return i if palindrome?(s[0...i]+s[i+1...s.size])
+    return j if palindrome?(s[0...j]+s[j+1...s.size])
+
+-i
+    
+end
+
+def palindrome?(s)
+    s == s.reverse
+   end
+
+
+palindromeIndex('bcbc')
